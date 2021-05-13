30655231abbcf6336d1d3ef952b501bf47e5e5a9
Olga Magrytska <olia.magritskaia@gmail.com>
2021-05-13 20:03:43 +0200

    the button scrollUp

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ window.addEventListener("DOMContentLoaded", () => {
 const modal = document.querySelector('.modal-callback'),
       modalOverlay = document.querySelector('.modal-overlay'),
       modalClose = document.querySelector('.modal-close'),
-      callbackBtn = document.querySelectorAll('.callback-btn');
-         
+      callbackBtn = document.querySelectorAll('.callback-btn');   
       
       
 
@@ -16,16 +15,18 @@ const modal = document.querySelector('.modal-callback'),
   })
    
  });
- const closeModal=()=>{
-  modal.style.display="none";
-   modalOverlay.style.display='none'
+   const closeModal=()=>{
+    modal.style.display="none";
+    modalOverlay.style.display='none'
  }
  modalClose.addEventListener('click',closeModal)
  modalOverlay.addEventListener('click',closeModal)
 
- const links = document.querySelectorAll('a[href^="#"]');
-  for(let elems of links){
-  elems.addEventListener('click', (e)=>{
+ // open sections using menu
+ const goToSections =()=>{
+  const links = document.querySelectorAll('a[href^="#"]');
+   for(let elems of links){
+    elems.addEventListener('click', (e)=>{
     e.preventDefault();    
     const id =elems.getAttribute('href');  
     document.querySelector(id).scrollIntoView({
@@ -35,8 +36,33 @@ const modal = document.querySelector('.modal-callback'),
     })
     
   })
-}
+ }
+ }
+ goToSections();
+ 
 
+// button scroll up
+const scrollUp =()=>{
+  const scrollBtn = document.querySelector('.up');
+  scrollBtn.style.display ='none';
+
+  window.onscroll = function(){ hideScrollBtn()}
+ const hideScrollBtn =()=>{
+  if(document.body.scrollTop >60 || document.documentElement.scrollTop >60){
+    scrollBtn.style.display="block"
+  }else{
+    scrollBtn.style.display='none'
+  }
+}
+    scrollBtn.addEventListener('click', (e)=>{
+    e.preventDefault();   
+    document.body.scrollTop = 0;
+     document.documentElement.scrollTop = 0;
+  })
+
+  
+}
+scrollUp();
 
 
 });
