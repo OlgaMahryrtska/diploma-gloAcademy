cd6aa12ac4deb3d91a1e9172f9150049fa86152c
Olga Magrytska <olia.magritskaia@gmail.com>
2021-05-17 10:17:25 +0200

    feedback form

--- script.js
+++ script.js
@@ -91,20 +91,82 @@ const showAccordeon =()=>{
         }else{
             let accElement =accItems[i].children[1];
            accElement.style.display='none'; 
-        }
-              
+        }              
       
      })
-    
-
    }
+ }
  
-  
+showAccordeon();
+// send form
+const sendForm =()=>{
+  const errorMes = 'Произошла ошибка...',
+  loadMessage = ' Загрузка...',
+  successMes = ' Спасибо!Вам скоро перезвонят!';
+const forms = document.getElementsByTagName('form');
+const statusMess = document.createElement('div');
+statusMess.textContent ="";
+statusMess.style.cssText="font-size:2rem; color:black";
+
+
+for(let item of forms){
+  item.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    item.appendChild(statusMess);
+    statusMess.textContent= loadMessage;
+    const formData = new FormData(item);
+    let body ={};
+    for(let val of formData.entries()){
+      body[val]=val[1];
+    }
+    postData(body)
+    .then((response)=>{
+      if(response.status !==200){
+        throw new Error('status network is not 200');
+      }
+      statusMess.textContent=successMes;
+      
+    })
+    .catch((error)=>{
+      statusMess.textContent= errorMes;
+      console.log(error)
+    })
+  })
+}
   
 
 }
- 
-showAccordeon();
+const postData =(body)=>{
+  return fetch('./server.php',{
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body)
+  })
+}
 
+sendForm()
+// check 
+const checkInputs =()=>{
+  const tel = document.querySelectorAll('.tel');
+const name = document.querySelectorAll('.name');
+name.forEach((names)=>{
+  names.addEventListener('input', (e)=>{
+    e.preventDefault();
+   names.value=names.value.replace(/[a-z]/gi, "");
+
+  })
+})
+tel.forEach((phones)=>{
+  phones.addEventListener('input', (e)=>{
+    e.preventDefault();
+  phones.value = phones.value.replace(/[^0-9\-+]/g, "")
+  })
+  
+})
+
+}
+checkInputs();
 });
 
