f2e7e88162a15f820c39e29bc111fcf9012acb37
Olga Magrytska <olia.magritskaia@gmail.com>
2021-05-18 10:26:31 +0200

    top-slider

--- script.js
+++ script.js
@@ -168,5 +168,54 @@ tel.forEach((phones)=>{
 
 }
 checkInputs();
+
+//slider carousel
+const carousel =()=>{
+  const arroLeft = document.querySelector('.arrow-left'),
+        arrowRight = document.querySelector('arrow-right'),
+        servicesCarousel =document.querySelector('.services-carousel'),
+        element = servicesCarousel.querySelectorAll('.element');
+
+}
+carousel();
+ 
+
+//slider
+const slider =()=>{
+  const slide = document.querySelectorAll('.item'),
+      slider = document.querySelector('.top-slider'),
+       title = document.querySelectorAll('.big');
+      console.log(title);
+    
+  let currentSlide = 0,
+  interval;
+  const prevSlide =(elem, index)=>{
+    elem[index].style.display="none";
+  }
+
+  const NextSlide =(elem, index)=>{
+    elem[index].style.display="block";
+
+  }
+
+  const autoPlaySlide =()=>{
+    prevSlide(slide, currentSlide);
+    currentSlide++;
+    if(currentSlide >=slide.length){
+      currentSlide =0;
+    }
+    NextSlide(slide, currentSlide);
+  }
+  const startSlide =(time=3000)=>{
+    interval = setInterval(autoPlaySlide, time)
+  }
+ 
+
+ startSlide(3000)
+  
+}
+ slider();
 });
+
+
 
